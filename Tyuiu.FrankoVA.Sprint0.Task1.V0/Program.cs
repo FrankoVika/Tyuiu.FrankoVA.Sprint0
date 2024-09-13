21522e7b2c85fbdee5fd073fc7fd972de15b2739
Tyuiu.FrankoVA@outlook.com <angelcosmo16@gmail.com>
2024-09-13 21:32:50 +0500

    Task1

---
+++ Tyuiu.FrankoVA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name = "Vika";
+int age = 19;
+Console.WriteLine("Здравствуйте меня зовут " + name + " Мне " + age + " лет");
+Console.ReadKey();
