0c2b4a1aee1ec6dc1b8c90dc8643369a0f8e4229
Tyuiu.FrankoVA@outlook.com <angelcosmo16@gmail.com>
2024-09-15 15:08:39 +0500

    Task7

---
+++ Tyuiu.FrankoVA.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.FrankoVA.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArrey = new int[5];
+            for (var i = 0; i < resultArrey.Length; i++)
+            {
+                resultArrey[i] = numOne[i] + numTwo[i];
+            }
+            return resultArrey;
+        }
+    }
+}
